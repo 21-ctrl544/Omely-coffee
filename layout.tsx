2df633930ce164ee5f80667bbf75111ae78a66d4
21-ctrl544 <2153801015125@email.hcmulaw.edu.vn>
2026-08-13 13:29:56 +0700

    Delete layout.tsx

--- layout.tsx
+++
@@ -1,3 +0,0 @@
-import "./globals.css";
-export const metadata={title:"OMELY COFFEE",description:"OMELY COFFEE QR Order"};
-export default function RootLayout({children}:{children:React.ReactNode}){return <html lang="vi"><body>{children}</body></html>}
